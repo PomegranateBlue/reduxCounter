b58c32ba0d0508792ce3b52be89889bf512f19bc
bluepome98 <wpdn1998@gmail.com>
2025-02-08 13:08:13 +0900

    useState를 이용한 카운터

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import { useState } from "react";
 function App() {
-  const [counter, setCounter] = useState();
+  const [counter, setCounter] = useState(0);
   return (
     <div>
       <h2>Redux Counter</h2>
+      <div value={counter}>counter:{counter}</div>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        ADD
+      </button>
+      <button
+        onClick={() => {
+          setCounter(counter - 1);
+        }}
+      >
+        SUB
+      </button>
     </div>
   );
 }
