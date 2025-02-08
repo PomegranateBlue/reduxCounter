67283b726cdd98b5cd41579f86fa7ff5f4b64aa3
bluepome98 <wpdn1998@gmail.com>
2025-02-08 13:03:39 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+function App() {
+  const [counter, setCounter] = useState();
+  return (
+    <div>
+      <h2>Redux Counter</h2>
+    </div>
+  );
+}
+
+export default App;
